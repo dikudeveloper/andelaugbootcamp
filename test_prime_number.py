de66dc4c304114cda4e368bf78b020b324d4d871
Joseph Diku <joseph.diku@gmail.com>
2017-04-18 01:34:46 +0300

    Modified test_prime_number.py
    Add a new test case to return error message if argument is 1

--- test_prime_number.py
+++ test_prime_number.py
@@ -8,16 +8,24 @@ class TestGeneratePrimeNumbers(unittest.TestCase):
     def setUp(self):
         self.pnum = generate_prime_numbers
 
-    def test_prime_number_non_integer(self):
+    def test_prime_number_returns_error_message_arg_non_integer(self):
         self.assertRaises(ValueError, self.pnum, 1.5)
 
-    def test_prime_number_negative(self):
+    def test_prime_number_returns_error_message_if_arg_negative(self):
         """should fail for negative prime numbers"""
         self.assertRaises(ValueError, self.pnum, -1)
 
-    def test_prime_number_not_zero(self):
+    def test_prime_number_returns_error_message_if_arg_zero(self):
         """should fail for 0 input"""
         self.assertRaises(ValueError, self.pnum, 0)
+
+    def test_prime_number_returns_error_message_if_arg_one(self):
+        """should fail for 1 input"""
+        self.assertRaises(ValueError, self.pnum, 1)
+
+    def test_prim_number_returns_error_message_if_arg_string(self):
+        """should fail for string input"""
+        self.assertRaises(ValueError, self.pnum, 'abc')
 
 if __name__ == '__main__':
     unittest.main()
