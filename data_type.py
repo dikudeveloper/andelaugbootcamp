82cabaa2d16c09542e510c2dc18e9bff7d00d164
Joseph Diku <joseph.diku@gmail.com>
2017-04-18 16:25:13 +0300

    Made Changes to data_type.py

--- data_type.py
+++ data_type.py
@@ -1,7 +1,20 @@
-def data_type(some_argument):
-    if isinstance(some_argument, str):
-        return len(some_argument)
-    elif isinstance(some_argument, int):
+def data_type(param):
+    if type(param) == str:
+        return len(param)
+    elif param is None:
         return 'no value'
-
-print(data_type(10))
+    elif type(param) == bool:
+        return param
+    elif type(param) == int:
+        if param < 100:
+            return 'less than 100'
+        elif param > 100:
+            return 'more than 100'
+        else:
+            return 'equal to 100'
+    else:
+        if type(param) == list:
+            if len(param) < 3:
+                return None
+            else:
+                return param[2]
